5507b50025c859832c0fb944f9ab23b36719edb6
Sean Kent <seanjkent2@gmail.com>
2025-12-03 23:54:07 -0800

    Made some improvements to the nav bar, but still have some cleanup work to do

--- include/nav-button.py
+++ include/nav-button.py
@@ -4,17 +4,18 @@ if "href" not in params:
 txt = ''
 txt += f'<div class="nav-button">\n'
 
-txt += f'    <a class="nav-button-text" href="{params["href"]}" style="padding-left: {params["indent"]}rem">\n'
-txt += f'        {params["text"]}\n'
-txt += f'    </a>\n'
 
 if params["toggle"] == "yes":
+    txt += f'    <a class="nav-button-text" href="{params["href"]}" style="padding-left: {params["indent"]}rem">\n'
+    txt += f'        {params["text"]}\n'
+    txt += f'    </a>\n'
     txt += f'    <div class="nav-button-chevron">\n'
     txt += jayweb.includef(f'{params["ROOT"]}/include/chevron-down.py', {"width": "1.35rem", "height": "1.35rem"}, 4)
     txt += f'    </div>\n'
 else:
-    txt += f'    <div>\n'
-    txt += f'    </div>\n'
+    txt += f'    <a class="nav-button-text nav-button-text-solo" href="{params["href"]}" style="padding-left: {params["indent"]}rem">\n'
+    txt += f'        {params["text"]}\n'
+    txt += f'    </a>\n'
 
 txt += f'</div>\n'
 
