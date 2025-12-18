26c29fb470bf62584ac9f12ce850a303bcd37740
Sean Kent <seanjkent2@gmail.com>
2025-12-17 17:38:58 -0800

    Got scrollbar to disappear when not scrolling

--- include/app.js
+++ include/app.js
@@ -175,9 +175,12 @@ OverlayScrollbars.plugin(ClickScrollPlugin);
 OverlayScrollbars(document.body, {
     scrollbars: {
         clickScroll: true,
-        visibility: 'auto',        // Change from 'visible' to 'auto'
-        autoHide: 'leave',         // Change from 'never' to 'leave', 'scroll', or 'move'
-        autoHideDelay: 1000,       // Delay in ms before hiding (default is 1300)
+        visibility: 'auto',
+        //visibility: 'hover',
+        //visibility: 'visible',
+        //autoHide: 'move',
+        autoHide: 'scroll',
+        autoHideDelay: 1000,
     },
 });
 
